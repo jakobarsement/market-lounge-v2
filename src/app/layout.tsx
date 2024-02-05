4aed8774d4e0b2ce1f27f9969aee39d632d21817
Jakob Arsement <jakobarsement@gmail.com>
2024-02-05 08:33:28 -0600

    Change background color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className="bg-primary-background">
         <PrimaryNavbar />
         {children}
       </body>
